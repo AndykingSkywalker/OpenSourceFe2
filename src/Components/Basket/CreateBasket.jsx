cbb7849c2e769393e8450ab508a891aad1a4cf38
Andrew King <andrewk640@gmail.com>
2024-03-27 20:25:39 +0000

    fixed responsiveness

--- src/Components/Basket/CreateBasket.jsx
+++ src/Components/Basket/CreateBasket.jsx
@@ -30,7 +30,7 @@ function CreateBasket() {
 
   return (
   <div className="card-group d-inline-flex padding" style={{ padding: "10px" }}>
-    <div class="card border-dark mb-3" style={{ width: "17%" }}>
+    <div class="card border-dark mb-3" style={{ width: "14rem", marginLeft:"0.2rem" }}>
       
     <form hidden={BasketExists} onSubmit={handleSubmit}>
       <div className="container" id="CreateBasket">
